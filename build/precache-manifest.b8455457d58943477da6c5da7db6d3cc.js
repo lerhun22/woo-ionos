cba5ab0257564cf1f95bd1b9bab2078f5bcbc42c
lerhun22 <didier.flury@wanadoo.fr>
2020-09-06 16:49:44 +0200

    first

---
+++ build/precache-manifest.b8455457d58943477da6c5da7db6d3cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb79a1a1e4cc67dcbc603f9249d8c17b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72b2041248f0054650c9",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e0e140732a4be3860a3d",
+    "url": "/static/js/2.011ffdff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.011ffdff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72b2041248f0054650c9",
+    "url": "/static/js/main.8ea62b74.chunk.js"
+  },
+  {
+    "revision": "872e677229d79872dca5",
+    "url": "/static/js/runtime-main.7d66edfd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
